c9b2ebdba455f0c87714aab6458e1e6358f4b9c6
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-08 12:51:13 +0530

    Lec-2

---
+++ IntroductionToJava/Lec-2/VariableDemo3.java
@@ -0,0 +1,12 @@
+public class VariableDemo3 {
+    public static void main(String[] args) {
+        int Age = 30;
+        int age = 30;
+        int marks = 90;
+        int _marks = 100;
+        int value = 500;
+        // int $value = 600;
+        int pay = 5000;
+        // int 9pay = 9000;
+    }
+}
