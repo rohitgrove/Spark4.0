209af4e7f64ecbf2ad26f9db129e70192b1028f2
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-08 13:20:45 +0530

    Lec-2

---
+++ IntroductionToJava/Lec-2/ExplicitCasting.java
@@ -0,0 +1,8 @@
+public class ExplicitCasting {
+    public static void main(String[] args) {
+        long sal = 7871978415646184l;
+        int a = (int) sal;
+        System.out.println(sal);
+        System.out.println(a);
+    }
+}
---
+++ IntroductionToJava/Lec-2/ImplicitCasting.java
@@ -0,0 +1,11 @@
+public class ImplicitCasting {
+    public static void main(String[] args) {
+        int a = 100;
+        long b = a;
+
+        short s = 12;
+        int c = s;
+
+        System.out.println(c);
+    }
+}
