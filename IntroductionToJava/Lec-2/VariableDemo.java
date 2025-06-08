6f034662a802124b9721d1467115d31c8a69a5ff
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-08 13:08:27 +0530

    remove

--- IntroductionToJava/Lec-2/VariableDemo.java
+++
@@ -1,8 +0,0 @@
-public class VariableDemo {
-    public static void main(String[] args) {
-        int salary = 10000;
-        String name = "Rohit";
-        System.out.println("My salary is = " + salary);
-        System.out.println("My name is = " + name);
-    }
-}
