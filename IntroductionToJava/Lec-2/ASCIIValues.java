d5811a07972c211298c80c9d28dfb774c9af9291
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-08 13:24:34 +0530

    Lec-2

---
+++ IntroductionToJava/Lec-2/ASCIIValues.java
@@ -0,0 +1,11 @@
+public class ASCIIValues {
+    public static void main(String[] args) {
+        for (int i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
+
+        for (int i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
