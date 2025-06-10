476db8e271de9d8f90932c0e0b20c2ab75a5a1c8
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-10 13:37:58 +0530

    OOPs

---
+++ Java/Opps/OPPs/ATM.java
@@ -0,0 +1,18 @@
+public class ATM {
+    private String accountHolderName;
+    private double balance;
+
+    public ATM(String name, double bal) {
+        this.accountHolderName = name;
+        this.balance = bal;
+    }
+
+    public static void main(String[] args) {
+        ATM atm = new ATM("Raj", 10000);
+        atm.showBal();
+    }
+
+    void showBal() {
+        System.out.println("You current bal = " + balance);
+    }
+}
---
+++ Java/Opps/OPPs/CalTest.java
@@ -0,0 +1,7 @@
+public class CalTest {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        int res = cal.add(10, 10);
+        System.out.println(res);
+    }
+}
