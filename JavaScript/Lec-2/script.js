e38c7fca8c7d96d8b1bad19aad57f0883a96490e
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-12 11:44:48 +0530

    Javasrcipt Lec-2

---
+++ JavaScript/Lec-2/script.js
@@ -0,0 +1,45 @@
+// var a = 10;
+// console.log(a);
+// console.log("Hi Rohit...");
+
+// console.warn("hello i'm warn");
+// console.error("error");
+// alert("hello world");
+
+// let name = prompt("Enter You name: ");
+// console.log(name);
+
+// var n = "rohit";
+// n = "jypsy";
+// console.log(n);
+
+// let a = 10;
+// a = 30;
+// console.log(a);
+
+// var y;
+// console.log(y);
+
+// var arr = [];
+// console.log(arr);
+
+
+// reflect in both
+// var arr = [1,2,3,4,5];
+// var arr1 = arr;
+
+// arr1.pop();
+
+// console.log(arr);
+// console.log(arr1);
+
+// isme jisme se bhi delete karenge toh sirf usme se delete hoga using spread
+var arr = [1,2,3,4,5];
+// is ko bolte hai spread
+var arr1 = [...arr];
+
+arr.pop();
+
+console.log(arr);
+console.log(arr1);
+
