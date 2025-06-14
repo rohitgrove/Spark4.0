f2444d43df07be95d5013e2aefb9cb5371ec369a
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-14 17:39:17 +0530

    lec-3

---
+++ JavaScript/Depth++/Lec-3/Logical.js
@@ -0,0 +1,29 @@
+// Logical operators
+// Note: it is used for combing multiple condition
+let users = "rohit";
+let isLoggedIn = true;
+let isPaid = false;
+
+if (isLoggedIn && isPaid) {
+  console.log("welcome to the codeforsuccess paid batch");
+} else {
+  console.log("you can access free workshop");
+}
+
+// ternary operator
+// note: short hand for if-else
+let age = 18;
+
+let canVote = age >= 18 ? "yes" : "no";
+console.log(canVote);
+let c1 = 10;
+console.log(c1++);
+console.log(typeof c1);
+console.log(!true);
+console.log(!false);
+
+let fname = "rohit";
+let lname = "grover";
+console.log(fname + " " + lname);
+
+let = `Hello ${fname} welcome to the CFS`;
---
+++ JavaScript/Depth++/Lec-3/operators.js
@@ -0,0 +1,22 @@
+let a = 10,
+  b = 30;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
+
+let c = 4;
+console.log(a ** c); // exponent => 10^4
+
+// Assignment opreators
+let k = 5;
+k = 6;
+k += 2;
+console.log(k);
+
+console.log(10 == "10"); // type check nhi karta
+console.log(10 === "10"); // type check karta hai
+console.log(10 > 4); // true
+console.log(6 <= 1); // false
