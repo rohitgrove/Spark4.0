3730728a5dea2de42f54df25ff6dc4eddfbab72b
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-06-16 13:19:59 +0530

    Multithreading

---
+++ Java/Multithreading/InterfaceDemo.java
@@ -0,0 +1,13 @@
+class MyTask implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("runnable....");
+    }
+}
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyTask());
+        t.start();
+    }
+}
