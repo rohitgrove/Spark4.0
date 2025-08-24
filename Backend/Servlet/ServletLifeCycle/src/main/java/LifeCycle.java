34e41b0edd7a95daf5769f3cc1b2769210761459
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-08-24 17:25:44 +0530

    ServletClass-1

---
+++ Backend/Servlet/ServletLifeCycle/src/main/java/LifeCycle.java
@@ -0,0 +1,26 @@
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LifeCycle extends GenericServlet {
+    @Override
+    public void init() {
+        System.out.println("init called - servlet initialized....");
+    }
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("service() called - Handling request");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2> Hello from Life Cycle </h2>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy() called - servlet is destroyed...");
+    }
+}
