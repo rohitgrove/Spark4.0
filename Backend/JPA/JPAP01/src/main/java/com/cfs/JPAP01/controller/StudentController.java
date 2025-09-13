194293e84439a96744dd63f256fde2f624009964
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-09-13 14:31:55 +0530

    JPA

---
+++ Backend/JPA/JPAP01/src/main/java/com/cfs/JPAP01/controller/StudentController.java
@@ -0,0 +1,49 @@
+package com.cfs.JPAP01.controller;
+
+import com.cfs.JPAP01.entity.Student;
+import com.cfs.JPAP01.repo.StudentRepo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepo studentRepo;
+    public StudentController(StudentRepo studentRepo)
+    {
+        this.studentRepo=studentRepo;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student)
+    {
+        return studentRepo.save(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents()
+    {
+        return studentRepo.findAll();
+    }
+
+    @PutMapping
+    public Student updateStudent(@RequestParam Long id, @RequestBody Student student)
+    {
+        Student s = studentRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("Student Not Found"));
+        s.setName(student.getName());
+        s.setEmail(student.getEmail());
+        return studentRepo.save(s);
+    }
+
+    @PatchMapping
+    public Student patchStudent(@RequestParam Long id, @RequestParam String name)
+    {
+        Student s = studentRepo.findById(id)
+                .orElseThrow(()->new RuntimeException("Student Not Found"));
+        s.setName(name);
+        return s;
+    }
+}
