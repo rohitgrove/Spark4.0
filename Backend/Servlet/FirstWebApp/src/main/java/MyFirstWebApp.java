f097b3e28dbf9012e9d30dd6b92a7305b090f123
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-11-27 16:22:46 +0530

    Sevlet

---
+++ Backend/Servlet/FirstWebApp/src/main/java/MyFirstWebApp.java
@@ -0,0 +1,17 @@
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MyFirstWebApp extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2> Hello from Rohit Grover <h2>");
+    }
+}
