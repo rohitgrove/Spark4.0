f0455c95dada1d9242c8dc03351f20787d0f6396
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-12-09 16:09:02 +0530

    SpringCore

---
+++ Backend/SpringCore/InjectionDemo/src/main/java/org/example/DSA.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class DSA implements Course {
+    @Override
+    public int enroll() {
+        System.out.print("DSA ");
+        return 1;
+    }
+}
---
+++ Backend/SpringCore/TightCoupling/src/EngineFactory.java
@@ -0,0 +1,11 @@
+public class EngineFactory {
+    public static Engine getEngine(String type) {
+        if (type.equalsIgnoreCase("disel")) {
+            return new DieselEngine();
+        } else if (type.equalsIgnoreCase("petrol")) {
+            return new PertolEngine();
+        } else {
+            throw new IllegalArgumentException("Invalid engine Type");
+        }
+    }
+}
