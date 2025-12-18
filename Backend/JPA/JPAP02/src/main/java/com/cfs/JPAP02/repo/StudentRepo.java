5624ac1f397e05fff378a22729256607702fcf55
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-12-18 12:37:48 +0530

    RemoveJPA

--- Backend/JPA/JPAP02/src/main/java/com/cfs/JPAP02/repo/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.cfs.JPAP02.repo;
-
-import com.cfs.JPAP02.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student,Long> {
-}
