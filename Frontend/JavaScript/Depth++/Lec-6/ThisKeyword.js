a07248ff0693ab6fe38f0cf779e2bee0a5aae32b
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-06-04 10:19:58 +0530

    FrontendRemoved

--- Frontend/JavaScript/Depth++/Lec-6/ThisKeyword.js
+++
@@ -1,8 +0,0 @@
-const person = {
-    name: "Aman",
-    welcome() {
-        console.log(`hello I am ${this.name}`);
-    }
-}
-person.welcome();
-// Note : this means current object
--- Frontend/JavaScript/Depth++/Lec-9/Async-wait.js
+++
@@ -1,8 +0,0 @@
-async function demo() {
-    // return "hello world";
-    return new Promise.resolve("hello world");
-}
-
-console.log("start");
-demo();
-console.log("end");
