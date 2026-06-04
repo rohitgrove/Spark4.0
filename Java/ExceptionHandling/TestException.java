397a6ec9bc3ef1e3d40eb383c9a51fc9fe01e27e
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-06-04 10:22:31 +0530

    ExceptionHandling

--- Java/ExceptionHandling/TestException.java
+++ Java/ExceptionHandling/TestException.java
@@ -2,6 +2,8 @@ public class TestException {
     public static void main(String[] args) {
         System.out.println("Hello Arjun");
         System.out.println("How are you");
+        // 1. Compiled successfully
+        // 2. Crashed at runtime with Exception
         System.out.println(10 / 0);
         System.out.println("Arjun : I am good");
     }
